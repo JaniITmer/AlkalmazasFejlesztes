92151d157d458bdae25b763bb820718cc8ce7b1d
adamgbad <adamgbad@gmail.com>
2023-10-04 12:33:00 +0200

    Program - kezdőlap megvalósítása

--- AlkalmazasFejlesztes_projekt/AlkalmazasFejlesztes_projekt/Form1.Designer.cs
+++ AlkalmazasFejlesztes_projekt/AlkalmazasFejlesztes_projekt/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(194, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Szerencsejáték szimulátor";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Játék véletlenszerű számokkal";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(491, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Játék saját számokkkal";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(304, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Kilépés";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Szerencsejatek_szimulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
