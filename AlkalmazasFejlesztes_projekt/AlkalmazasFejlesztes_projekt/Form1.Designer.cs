7f86b2255f3370318bf90b75c874241f6680a9a5
JaniITmer <batyus4563@gmail.com>
2023-10-05 12:39:07 +0200

    design módosítás

--- AlkalmazasFejlesztes_projekt/AlkalmazasFejlesztes_projekt/Form1.Designer.cs
+++ AlkalmazasFejlesztes_projekt/AlkalmazasFejlesztes_projekt/Form1.Designer.cs
@@ -38,51 +38,60 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(194, 48);
+            this.label1.Location = new System.Drawing.Point(146, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(400, 38);
+            this.label1.Size = new System.Drawing.Size(332, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Szerencsejáték szimulátor";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(75, 149);
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(56, 121);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(212, 23);
+            this.button1.Size = new System.Drawing.Size(179, 44);
             this.button1.TabIndex = 1;
             this.button1.Text = "Játék véletlenszerű számokkal";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(491, 149);
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(364, 121);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(212, 23);
+            this.button2.Size = new System.Drawing.Size(179, 44);
             this.button2.TabIndex = 2;
             this.button2.Text = "Játék saját számokkkal";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(304, 331);
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(227, 258);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(186, 23);
+            this.button3.Size = new System.Drawing.Size(157, 29);
             this.button3.TabIndex = 3;
             this.button3.Text = "Kilépés";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // MainOldal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::AlkalmazasFejlesztes_projekt.Properties.Resources._1554823612_7Q8WUqLVF_md;
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainOldal";
             this.Text = "Szerencsejatek_szimulator";
             this.ResumeLayout(false);
